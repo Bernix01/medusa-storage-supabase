0b69544952ee368473409e21f5370fe4f7607072
Bernix01 <gbernal096@gmail.com>
2024-02-01 18:15:13 -0500

    base implementation

---
+++ src/services/supabase.ts
@@ -0,0 +1,136 @@
+import { AbstractFileService, Logger, MedusaContainer } from "@medusajs/medusa";
+import {
+  DeleteFileType,
+  FileServiceGetUploadStreamResult,
+  FileServiceUploadResult,
+  GetUploadedFileType,
+  UploadStreamDescriptorType,
+} from "@medusajs/types";
+import { StorageClient } from "@supabase/storage-js";
+
+class SupabaseFileService extends AbstractFileService {
+  logger: Logger;
+  storageClient: StorageClient;
+  bucket: string;
+  signedUrlExpiration = 120;
+
+  constructor(
+    container: MedusaContainer,
+    { serviceKey, bucketName, referenceID }: Record<string, unknown>
+  ) {
+    super(container);
+
+    this.logger = container.cradle.logger;
+    this.bucket = bucketName as string;
+    this.storageClient = new StorageClient(
+      `https://${referenceID}.supabase.co/storage/v1`,
+      {
+        apikey: serviceKey as string,
+        Authorization: `Bearer ${serviceKey as string}`,
+      }
+    );
+  }
+
+  async upload(
+    fileData: Express.Multer.File
+  ): Promise<FileServiceUploadResult> {
+    const { data, error } = await this.storageClient
+      .from(this.bucket)
+      .upload("assets", fileData.stream);
+
+    if (error) {
+      this.logger.error(error);
+      throw new Error("Error uploading file");
+    }
+
+    const signedURLResult = await this.storageClient
+      .from(this.bucket)
+      .createSignedUrl(data.path, this.signedUrlExpiration);
+
+    if (signedURLResult.error) {
+      this.logger.error(signedURLResult.error);
+      throw new Error("Error getting presigned url");
+    }
+
+    return {
+      key: data.path,
+      url: signedURLResult.data.signedUrl,
+    };
+  }
+
+  async uploadProtected(
+    fileData: Express.Multer.File
+  ): Promise<FileServiceUploadResult> {
+    const { data, error } = await this.storageClient
+      .from(this.bucket)
+      .upload("assets", fileData.stream);
+
+    if (error) {
+      this.logger.error(error);
+      throw new Error("Error uploading file");
+    }
+
+    const signedURLResult = await this.storageClient
+      .from(this.bucket)
+      .createSignedUrl(data.path, this.signedUrlExpiration);
+
+    if (signedURLResult.error) {
+      this.logger.error(signedURLResult.error);
+      throw new Error("Error getting presigned url");
+    }
+
+    return {
+      key: data.path,
+      url: signedURLResult.data.signedUrl,
+    };
+  }
+
+  async delete(fileData: DeleteFileType): Promise<void> {
+    const { error } = await this.storageClient
+      .from(this.bucket)
+      .remove([fileData.fileKey]);
+
+    if (error) {
+      this.logger.error(error);
+      throw new Error("Error deleting file");
+    }
+  }
+
+  async getUploadStreamDescriptor(
+    fileData: UploadStreamDescriptorType
+  ): Promise<FileServiceGetUploadStreamResult> {
+    throw new Error("Method not implemented.");
+  }
+
+  async getDownloadStream(
+    fileData: GetUploadedFileType
+  ): Promise<NodeJS.ReadableStream> {
+    const { data, error } = await this.storageClient
+      .from(this.bucket)
+      .download(fileData.fileKey);
+
+    if (error) {
+      this.logger.error(error);
+      throw new Error("Error getting download stream");
+    }
+
+    return data.stream();
+  }
+
+  async getPresignedDownloadUrl(
+    fileData: GetUploadedFileType
+  ): Promise<string> {
+    const { data, error } = await this.storageClient
+      .from(this.bucket)
+      .createSignedUrl(fileData.fileKey, this.signedUrlExpiration);
+
+    if (error) {
+      this.logger.error(error);
+      throw new Error("Error getting presigned url");
+    }
+
+    return data.signedUrl;
+  }
+}
+
+export default SupabaseFileService;
